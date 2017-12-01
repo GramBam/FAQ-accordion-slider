e41a35ba16b52f8af6380ef66b97163bbc42677f
GramBam <graham.moss@hotmail.com>
2017-11-30 21:22:00 -0500

    Make current answer close when you open a new one

--- js/script.js
+++ js/script.js
@@ -7,6 +7,8 @@ var speed="500";
 $(document).ready(function() {
   $('li.q').on(action ,function(){
     $(this).next()
-    .slideToggle(speed);
+    .slideToggle(speed)
+    .siblings('li.a')
+    .slideUp()
   });
 });
