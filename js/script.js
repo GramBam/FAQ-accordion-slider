b35debe19c4d5018fdeccd42ebe2c7626ee0c2fd
GramBam <graham.moss@hotmail.com>
2017-11-30 21:18:10 -0500

    Add javascript to slide toggle answer

---
+++ js/script.js
@@ -0,0 +1,12 @@
+// Accordion
+
+var action="click";
+
+var speed="500";
+
+$(document).ready(function() {
+  $('li.q').on(action ,function(){
+    $(this).next()
+    .slideToggle(speed);
+  });
+});
